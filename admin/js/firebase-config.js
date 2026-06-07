4ce9bca427f3add4b1bd03078d223ea0c7027add
sp-contraction-dot-com <spbuild.official@gmail.com>
2026-06-07 10:12:30 +0530

    Create js/firebase-config.js

---
+++ admin/js/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAT3LQUFBhiZINM9NZTulX7umf-P1Y6i2I",
+    authDomain: "sp-build-official.firebaseapp.com",
+    projectId: "sp-build-official",
+    storageBucket: "sp-build-official.firebasestorage.app",
+    messagingSenderId: "377403616440",
+    appId: "1:377403616440:web:bfae56e748f05de22a184d",
+    measurementId: "G-P319FN0C0N",
+    databaseURL: "https://sp-build-official-default-rtdb.firebaseio.com/"
+};
